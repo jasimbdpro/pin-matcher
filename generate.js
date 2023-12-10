5de562b4a1660d1a20812e924b1fa84cbb253088
jasimbdpro <jasimbdpro@gmail.com>
2023-12-10 15:03:55 +0600

    changes

--- generate.js
+++ generate.js
@@ -1,3 +1,4 @@
+//Generate part
 let randomNumbers = [];
 for (let i = 0; i < 4; i++) {
     let randomNumber = Math.floor(Math.random() * 10)
@@ -9,3 +10,22 @@ let myGeneration = document.getElementById('myGeneration')
 generateButton.addEventListener('click', function () {
     myGeneration.value = generatedNumber
 })
+
+// Submit part
+const calcBody = document.querySelectorAll('div.button')
+const myInput = document.getElementById('myInput')
+calcBody.forEach((button) => {
+    button.addEventListener('click', e => {
+        myInputValue(e.target.dataset.value)
+    })
+})
+const myInputValue = (myValue) => {
+    if (myValue === 'C') return myInput.value = '';
+    else if (myValue === '<') return myInput.value = myInput.value.toString().slice(0, -1);
+    else if (myInput.value !== '' && myValue !== '<' && myValue !== 'C') {
+        myInput.value = myInput.value + myValue
+    }
+    else {
+        myInput.value = myValue;
+    }
+}
