c6fa452f1ffb26d6ea16422c9fed1842ebed2cd6
jasimbdpro <jasimbdpro@gmail.com>
2023-12-10 15:00:59 +0600

    Changes

--- submit.js
+++ submit.js
@@ -2,6 +2,16 @@ const calcBody = document.querySelectorAll('div.button')
 const myInput = document.getElementById('myInput')
 calcBody.forEach((button) => {
     button.addEventListener('click', e => {
-        myInput.value = (e.target.dataset.value)
+        myInputValue(e.target.dataset.value)
     })
 })
+const myInputValue = (myValue) => {
+    if (myValue === 'C') return myInput.value = '';
+    else if (myValue === '<') return myInput.value = myInput.value.toString().slice(0, -1);
+    else if (myInput.value !== '' && myValue !== '<' && myValue !== 'C') {
+        myInput.value = myInput.value + myValue
+    }
+    else {
+        myInput.value = myValue;
+    }
+}
